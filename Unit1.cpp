b70656ad424065967ab62909848db2073dad50e9
zdoonio <dominik.zdunczyk@gmail.com>
2016-12-23 16:07:26 +0100

    COMPLETE NOTEPAD

---
+++ Unit1.cpp
@@ -0,0 +1,199 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+#include "Unit2.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+
+AnsiString nazwapliku="";
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+
+
+
+
+
+void __fastcall TForm1::Otwrz1Click(TObject *Sender)
+{
+if (OpenDialog1->Execute())
+{
+        try{
+                tresc->Lines->LoadFromFile(OpenDialog1->FileName);
+                nazwapliku = OpenDialog1->FileName;
+        }
+
+        catch(...){
+                ShowMessage("B³¹d otwarcia pliku. Upewnij siê, ¿e plik istnieje na dysku");
+        }
+}
+}
+//---------------------------------------------------------------------------
+
+
+
+
+void __fastcall TForm1::Zapisz1Click(TObject *Sender)
+{
+if (nazwapliku!="")
+{
+        tresc->Lines->SaveToFile(nazwapliku);
+}
+
+else
+{
+        Form1->Zapiszjako1Click(MainMenu1);
+}
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Zapiszjako1Click(TObject *Sender)
+{
+       if (SaveDialog1->Execute())
+{
+        try{
+                tresc->Lines->SaveToFile(SaveDialog1->FileName);
+                nazwapliku = OpenDialog1->FileName;
+        }
+
+        catch(...){
+                ShowMessage("B³¹d zapisu pliku.");
+        }
+}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Nowy1Click(TObject *Sender)
+{
+        if(Application->MessageBox(
+        "Czy na pewno utworzyc nowy plik?", "Potwierdz",
+        MB_YESNOCANCEL | MB_ICONQUESTION) == IDYES)
+        {
+        tresc->Lines->Clear();
+        nazwapliku="";
+        }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::trescKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        if (Shift.Contains(ssCtrl))
+        {
+                if ((Key =='s') || (Key == 'S'))
+                {
+                        Form1->Zapisz1Click(MainMenu1);
+                }
+                 if ((Key =='a') || (Key == 'A'))
+                {
+                        tresc->SelectAll();
+                }
+                 if ((Key =='n') || (Key == 'N'))
+                {
+                        Form1->Nowy1Click(MainMenu1);
+                }
+        }
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Zakocz1Click(TObject *Sender)
+{
+
+  if(Application->MessageBox(
+        "Czy na pewno chcesz wyjsc z programu?", "Potwierdz",
+        MB_YESNOCANCEL | MB_ICONQUESTION) == IDYES)
+        {
+        Application -> Terminate();
+        }
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormClose(TObject *Sender, TCloseAction &Action)
+{
+      if(Application->MessageBox(
+        "Czy na pewno chcesz wyjsc z programu?", "Potwierdz",
+        MB_YESNOCANCEL | MB_ICONQUESTION) == IDNO)
+        {
+        Action = caNone;
+        }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Wytnijctrlx1Click(TObject *Sender)
+{
+        tresc->CutToClipboard();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::KopiujCtrlc1Click(TObject *Sender)
+{
+      tresc->CopyToClipboard();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::WklejCtrlv1Click(TObject *Sender)
+{
+    tresc->PasteFromClipboard();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::ZawijanieWierszy1Click(TObject *Sender)
+{
+        if (ZawijanieWierszy1 -> Checked == true)
+        {
+                ZawijanieWierszy1 ->Checked = false;
+                tresc->ScrollBars = ssBoth;
+                tresc->WordWrap = false;
+                }
+
+        else
+        {
+        ZawijanieWierszy1 -> Checked=true;
+        tresc -> ScrollBars = ssVertical;
+        tresc->WordWrap = true;
+        }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Czcionka1Click(TObject *Sender)
+{
+        if(FontDialog1->Execute())
+        {
+                //kroj czcionki
+                tresc->Font->Name=  FontDialog1->Font->Name;
+
+                //kolor czcionki
+                tresc->Font->Color=  FontDialog1->Font->Color;
+
+                //rozmiar czcionki
+                tresc->Font->Size=  FontDialog1->Font->Size;
+
+                //styl
+                tresc->Font->Style=  FontDialog1->Font->Style;
+        }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Oprogramie1Click(TObject *Sender)
+{
+ Form2->ShowModal();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Zapraszamnabloga1Click(TObject *Sender)
+{
+        ShellExecute(NULL, "open", "http://miroslawzelent.pl", NULL, NULL, SW_SHOWNORMAL);        
+}
+//---------------------------------------------------------------------------
+
